1c99d86e81d80745cce91ae5ddb2695183d5a477
Tudor Paraschivescu <tudorp97@gmail.com>
2018-05-16 14:40:10 +0300

    Add AtmelStudio project

---
+++ atmega324-memory-game/main.c
@@ -0,0 +1,18 @@
+/*
+ * atmega324-memory-game.c
+ *
+ * Created: 16/05/2018 14:25:37
+ * Author : Tudor
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
